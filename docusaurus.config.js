f7432578f0619aa7f579d1c8eeb10132819c65ef
~$ ashorterm <98740335+aspsptyd@users.noreply.github.com>
2024-07-04 17:34:27 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://aspsptyd.github.io',
+  trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dev-panels/',
