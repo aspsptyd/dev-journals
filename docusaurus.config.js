401014bd3c1c0fec29a2fe10140bad98750044f4
aspsptyd <devadiroot@gmail.com>
2024-07-17 10:34:54 +0700

    Fix: baseURL not match

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dev-panels/',
+  baseUrl: '/dev-journals/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
